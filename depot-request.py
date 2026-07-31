e669c27db5f124b1a9aeb3eb66e9bd1bf1725a26
gwigzz <grdevphp@gmail.com>
2026-07-31 17:20:22 +0200

    add version

---
+++ depot-request.py
@@ -0,0 +1,12 @@
+# test request to repo for checking version app
+
+import requests
+
+print("test")
+
+def check_last_version():
+    """ return online version from github repo """
+
+
+def check_current_version():
+    """ return current local version from computeur """
